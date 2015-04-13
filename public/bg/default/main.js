ce685bdd340f01dec12f41342b29c3fcb35ef0aa
Yuan Chiu <chyuaner@gmail.com>
2015-04-14 04:22:12 +0800

    背景改良

--- public/bg/default/main.js
+++ public/bg/default/main.js
@@ -1,15 +1,17 @@
 function BackGround(){
-  this.bg_geometry = new THREE.BoxGeometry(1, 10, 20);
+  this.bg_geometry = new THREE.CylinderGeometry(3, 3, 100);
   this.bg_texture = THREE.ImageUtils.loadTexture('/bg/default/track.jpg');
+  this.bg_texture.rotation = THREE.Math.degToRad(90);
   this.bg_material = new THREE.MeshBasicMaterial({ map: this.bg_texture });
   this.bg_material.side = THREE.DoubleSide;
   this.bg_cube = new THREE.Mesh(this.bg_geometry, this.bg_material);
+  this.bg_cube.rotation.x = THREE.Math.degToRad(90);
 
   this.getMesh = function() {
     return this.bg_cube;
   }
 
   this.aniNext = function() {
-    this.bg_texture.offset.set((s * 0.01) % 1);
+    this.bg_texture.offset.y = -((s * 0.02) % 1);
   }
 }
