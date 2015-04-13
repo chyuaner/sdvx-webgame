75a446abe6e72f4deca3dbb3eb1a2be34c8f3b2c
Yuan Chiu <chyuaner@gmail.com>
2015-04-14 03:28:52 +0800

    校正 BT FX

--- public/js/game_base.js
+++ public/js/game_base.js
@@ -1,4 +1,4 @@
-var offset = window.innerHeight*0.6; // 調整軌道高度
+var offset = window.innerHeight*0.8; // 調整軌道高度
 
 // 建立場景
 var scene = new THREE.Scene();
@@ -6,8 +6,8 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHe
 
 // 建立畫布
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight + offset);
-renderer.setViewport (0, offset, window.innerWidth, window.innerHeight + offset);
+renderer.setSize( window.innerWidth, window.innerHeight);
+renderer.setViewport (0, 0, window.innerWidth, window.innerHeight + offset);
 document.body.appendChild(renderer.domElement);
 
 // 建立背景
@@ -21,42 +21,57 @@ scene.add(track);
 // 軌道位置
 track.position.y = -1;
 
-// 建立其中白音符
-var bt_geometry = new THREE.BoxGeometry(0.15, 0.05, 0.01);
+// 建立其中bt白音符
+var bt_geometry = new THREE.BoxGeometry(0.15, 0.01, 0.025);
 var bt_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 var bt1_cube = new THREE.Mesh(bt_geometry, bt_material);
 var bt2_cube = new THREE.Mesh(bt_geometry, bt_material);
 var bt3_cube = new THREE.Mesh(bt_geometry, bt_material);
 var bt4_cube = new THREE.Mesh(bt_geometry, bt_material);
+bt1_cube.position.y = -0.98;
+bt2_cube.position.y = -0.98;
+bt3_cube.position.y = -0.98;
+bt4_cube.position.y = -0.98;
+bt1_cube.position.x = -0.25;
+bt2_cube.position.x = -0.086;
+bt3_cube.position.x =  0.078;
+bt4_cube.position.x =  0.242;
 scene.add(bt1_cube);
 scene.add(bt2_cube);
 scene.add(bt3_cube);
 scene.add(bt4_cube);
 
-bt1_cube.position.y = -0.9999;
-bt2_cube.position.y = -0.9999;
-bt3_cube.position.y = -0.9999;
-bt4_cube.position.y = -0.9999;
-bt1_cube.position.x = -0.25;
-bt2_cube.position.x = -0.086;
-bt3_cube.position.x =  0.078;
-bt4_cube.position.x =  0.242;
 
-// 白鍵軌道位置
+// 建立其中fx黃音符
+var fx_geometry = new THREE.BoxGeometry(0.313, 0.01, 0.025);
+var fx_material = new THREE.MeshBasicMaterial({ color: 0xff8e00 });
+var fx1_cube = new THREE.Mesh(fx_geometry, fx_material);
+var fx2_cube = new THREE.Mesh(fx_geometry, fx_material);
+fx1_cube.position.y = -0.9999;
+fx2_cube.position.y = -0.9999;
+fx1_cube.position.x = -0.169;
+fx2_cube.position.x =  0.16;
+scene.add(fx1_cube);
+scene.add(fx2_cube);
+
+// 軌道位置
 bt1_cube.position.z = -10;  // 起始
 bt2_cube.position.z = 3;
 bt3_cube.position.z = 4.73; // 平面軌道結束
 bt4_cube.position.z = 5.16; // 傾斜軌道結束
+fx1_cube.scale.z = 80.51;
+fx1_cube.position.z = 2;
+fx2_cube.position.z = 2;
 
 // 建立背景
 scene.add(bg.getMesh());
 
-// 視角傾斜度(正為左傾斜)
+// 視角傾斜度(正為左傾斜，SDVXII最高傾斜度20)
 camera.rotation.z = THREE.Math.degToRad(0);
 // 視角垂直傾斜度(以負為主)
 camera.rotation.x = THREE.Math.degToRad(0);
 // 視角上下遠近(數字越大越遠)
-camera.position.y = 0
+camera.position.y = 0;
 // 視角遠近
 camera.position.z = 6;
 
@@ -68,7 +83,7 @@ var render = function () {
 
     s += 0.2;
 
-    // camera.rotation.z = Math.cos(s * 0.1) * 0.8
+    camera.rotation.z = Math.cos(s * 0.1) * THREE.Math.degToRad(20);
 
     // bt2_cube.position.z =  Math.cos(s * 0.05) * 4.5;
 
