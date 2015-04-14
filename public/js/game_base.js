b5becb7ec987e0bb2cd6d16f58a7ba725641676c
Yuan Chiu <chyuaner@gmail.com>
2015-04-14 20:47:18 +0800

    add 判定線

--- public/js/game_base.js
+++ public/js/game_base.js
@@ -1,4 +1,4 @@
-var offset = window.innerHeight*0.8; // 調整軌道高度
+var offset = window.innerHeight*0.7; // 調整軌道高度
 
 // 建立場景
 var scene = new THREE.Scene();
@@ -21,9 +21,22 @@ scene.add(track);
 // 軌道位置
 track.position.y = -1;
 
+// 判定線
+var judge_line_geometry = new THREE.CylinderGeometry(0.015, 0.015, 1.1);
+var judge_line_material = new THREE.MeshBasicMaterial({ color: 0xFF00A6 });
+var judge_line = new THREE.Mesh(judge_line_geometry, judge_line_material);
+judge_line.rotation.z = THREE.Math.degToRad(90);
+judge_line.position.y = -0.9;
+judge_line.position.z = 4.16;
+scene.add(judge_line);
+// 判定線左右偏移(+-0.07)
+judge_line.position.x = 0;
+
 // 建立其中bt白音符
-var bt_geometry = new THREE.BoxGeometry(0.15, 0.01, 0.025);
+var bt_geometry = new THREE.BoxGeometry(0.15, 0.01, 0.09);
 var bt_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+bt_material.transparent = true;
+bt_material.opacity = 0.8;
 var bt1_cube = new THREE.Mesh(bt_geometry, bt_material);
 var bt2_cube = new THREE.Mesh(bt_geometry, bt_material);
 var bt3_cube = new THREE.Mesh(bt_geometry, bt_material);
@@ -43,8 +56,10 @@ scene.add(bt4_cube);
 
 
 // 建立其中fx黃音符
-var fx_geometry = new THREE.BoxGeometry(0.313, 0.01, 0.025);
+var fx_geometry = new THREE.BoxGeometry(0.313, 0.01, 0.09);
 var fx_material = new THREE.MeshBasicMaterial({ color: 0xff8e00 });
+fx_material.transparent = true;
+fx_material.opacity = 0.8;
 var fx1_cube = new THREE.Mesh(fx_geometry, fx_material);
 var fx2_cube = new THREE.Mesh(fx_geometry, fx_material);
 fx1_cube.position.y = -0.9999;
@@ -55,13 +70,13 @@ scene.add(fx1_cube);
 scene.add(fx2_cube);
 
 // 軌道位置
-bt1_cube.position.z = -10;  // 起始
-bt2_cube.position.z = 3;
-bt3_cube.position.z = 4.73; // 平面軌道結束
+// -10, 4, 4.73, 5.16
+bt1_cube.position.z = -2;  // 起始
+bt2_cube.position.z = 2.8;
+bt3_cube.position.z = 3.8; // 平面軌道結束
 bt4_cube.position.z = 5.16; // 傾斜軌道結束
-fx1_cube.scale.z = 80.51;
 fx1_cube.position.z = 2;
-fx2_cube.position.z = 2;
+fx2_cube.position.z = 1.4;
 
 // 建立背景
 scene.add(bg.getMesh());
@@ -84,7 +99,7 @@ var render = function () {
     s += 0.2;
 
     camera.rotation.z = Math.cos(s * 0.1) * THREE.Math.degToRad(20);
-
+    judge_line.position.x = Math.cos(s * 0.5) * 0.07;
     // bt2_cube.position.z =  Math.cos(s * 0.05) * 4.5;
 
     renderer.render(scene, camera);
